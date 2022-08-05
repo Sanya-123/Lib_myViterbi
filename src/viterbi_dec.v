6d62a981a930dfea4cc62d674d6eb212306c643b
Alexandr Krivtsun <alexandr.krivtsun@gmail.com>
2022-08-05 16:00:20 +0700

    add support fix (some) error on speed 2/3, 3/4, 4/5 ....

--- src/viterbi_dec.v
+++ src/viterbi_dec.v
@@ -27,44 +27,32 @@ module viterbi_dec #(parameter p_size_polinom = 3, parameter p_polinom_0 = 3'b11
     i_valid,
     o_data,
     o_valid,
-    o_error,
-    o_data_d1,
-    o_valid_d1
+    o_error
     );
-
-    input i_clk;
-    input i_reset;
-    input [1:0] i_data;
-    input [1:0] i_valid;
-    output o_data;
-    output o_valid;
-    output o_error;
-    output o_data_d1;
-    output o_valid_d1;
-
 
     localparam p_defoult_in_0_0 = ^({p_defoult_state, 1'b0} & p_polinom_0);
     localparam p_defoult_in_0_1 = ^({p_defoult_state, 1'b0} & p_polinom_1);
     localparam p_defoult_in_1_0 = ^({p_defoult_state, 1'b1} & p_polinom_0);
     localparam p_defoult_in_1_1 = ^({p_defoult_state, 1'b1} & p_polinom_1);
 
-    localparam p_defoult_in_00_0 = ^({p_defoult_state, 1'b00} & p_polinom_0);
-    localparam p_defoult_in_00_1 = ^({p_defoult_state, 1'b00} & p_polinom_1);
-    localparam p_defoult_in_01_0 = ^({p_defoult_state, 1'b01} & p_polinom_0);
-    localparam p_defoult_in_01_1 = ^({p_defoult_state, 1'b01} & p_polinom_1);
-    localparam p_defoult_in_10_0 = ^({p_defoult_state, 1'b10} & p_polinom_0);
-    localparam p_defoult_in_10_1 = ^({p_defoult_state, 1'b10} & p_polinom_1);
-    localparam p_defoult_in_11_0 = ^({p_defoult_state, 1'b11} & p_polinom_0);
-    localparam p_defoult_in_11_1 = ^({p_defoult_state, 1'b11} & p_polinom_1);
+    input i_clk;
+    input i_reset;
+    input [1:0] i_data;
+    input [1:0] i_valid;
+    output [p_size_polinom-1:0] o_data;
+    output [p_size_polinom-1:0] o_valid;
+    output o_error;
 
 
+    /********************************************************************************/
+    /* Regs and wires */
+    /********************************************************************************/
+     
     wire    [1:0]                   w_idata = i_data & i_valid;
-    reg                             r_odata = 1'b0;
-    reg                             r_valid = 1'b0;
+    reg     [p_size_polinom-1:0]    r_odata = 0;
+    reg     [p_size_polinom-1:0]    r_valid = 0;
 
     //data decoding after find error
-    reg                             r_odata_d1 = 1'b0;
-    reg                             r_valid_d1 = 1'b0;
     reg     [p_size_polinom-1:0]    r_shift_in = p_defoult_state;
 
     // code data if input 0 and 1
@@ -74,29 +62,38 @@ module viterbi_dec #(parameter p_size_polinom = 3, parameter p_polinom_0 = 3'b11
     // shift data if input 0 and 1
     wire    [p_size_polinom-1:0]    w_shift_in_0 = {r_shift_in, 1'b0};
     wire    [p_size_polinom-1:0]    w_shift_in_1 = {r_shift_in, 1'b1};
-    wire    [p_size_polinom-1:0]    w_shift_in_00 = {r_shift_in, 1'b0, 1'b0};
-    wire    [p_size_polinom-1:0]    w_shift_in_01 = {r_shift_in, 1'b0, 1'b1};
-    wire    [p_size_polinom-1:0]    w_shift_in_10 = {r_shift_in, 1'b1, 1'b0};
-    wire    [p_size_polinom-1:0]    w_shift_in_11 = {r_shift_in, 1'b1, 1'b1};
+    wire    [p_size_polinom-1:0]    w_shift_in_xx   [3:0];
 
     //fix erro reg
-    reg                             r_flag_error = 0;
+    reg     [p_size_polinom-1:0]    r_flag_error = 0;
     // code data if input 00 01 10 11
-    reg     [1:0]                   r_mod_in_00 = {p_defoult_in_00_1, p_defoult_in_00_0};
-    reg     [1:0]                   r_mod_in_01 = {p_defoult_in_01_1, p_defoult_in_01_0};
-    reg     [1:0]                   r_mod_in_10 = {p_defoult_in_10_1, p_defoult_in_10_0};
-    reg     [1:0]                   r_mod_in_11 = {p_defoult_in_11_1, p_defoult_in_11_0};
+    // reg     [1:0]                   r_mod_in_xx     [3:0];
 
     // shift data if input 000 to 111
-    wire    [p_size_polinom-1:0]    w_shift_in_000 = {r_shift_in, 1'b0, 1'b0, 1'b0};
-    wire    [p_size_polinom-1:0]    w_shift_in_001 = {r_shift_in, 1'b0, 1'b0, 1'b1};
-    wire    [p_size_polinom-1:0]    w_shift_in_010 = {r_shift_in, 1'b0, 1'b1, 1'b0};
-    wire    [p_size_polinom-1:0]    w_shift_in_011 = {r_shift_in, 1'b0, 1'b1, 1'b1};
-    wire    [p_size_polinom-1:0]    w_shift_in_100 = {r_shift_in, 1'b1, 1'b0, 1'b0};
-    wire    [p_size_polinom-1:0]    w_shift_in_101 = {r_shift_in, 1'b1, 1'b0, 1'b1};
-    wire    [p_size_polinom-1:0]    w_shift_in_110 = {r_shift_in, 1'b1, 1'b1, 1'b0};
-    wire    [p_size_polinom-1:0]    w_shift_in_111 = {r_shift_in, 1'b1, 1'b1, 1'b1};
+    // wire    [p_size_polinom-1:0]    w_shift_in_xxx  [7:0];
 
+    //TODO
+    reg     [p_size_polinom*2-1:0]  r_mod_in_0d0;
+    reg     [p_size_polinom*2-1:0]  r_mod_in_0d1;
+    reg     [p_size_polinom*2-1:0]  r_mod_in_1d0;
+    reg     [p_size_polinom*2-1:0]  r_mod_in_1d1;
+
+    reg     [p_size_polinom*2-1:0]  r_in_data_old;
+    reg     [p_size_polinom*2-1:0]  r_in_data_mask;
+    wire    [p_size_polinom*2-1:0]  w_in_data_shift = {r_in_data_old, w_idata};
+    wire    [p_size_polinom*2-1:0]  w_in_mask_shift = {r_in_data_mask, i_valid};
+
+    reg     [p_size_polinom-1:0]    r_decod_in_0dx;
+    reg     [p_size_polinom-1:0]    r_decod_in_1dx;
+
+    reg     [p_size_polinom-1:0]    r_shift_in_0dx = p_defoult_state;
+    reg     [p_size_polinom-1:0]    r_shift_in_1dx = p_defoult_state;
+    wire    [p_size_polinom-1:0]    w_shift_in_xdx  [3:0];
+    wire    [p_size_polinom-1:0]    w_shift_in_xdxx [7:0];
+
+    /********************************************************************************/
+    /* END Regs and wires */
+    /********************************************************************************/
 
     /********************************************************************************/
     /* Assign */
@@ -104,12 +101,33 @@ module viterbi_dec #(parameter p_size_polinom = 3, parameter p_polinom_0 = 3'b11
      
     assign o_data           = r_odata;
     assign o_valid          = r_valid;
-    assign o_data_d1        = r_odata_d1;
-    assign o_valid_d1       = r_valid_d1;
     assign o_error          = r_flag_error;
+
+    genvar i_xx, i_xxx, i_xdxx;
+    generate 
+    for(i_xx = 0 ; i_xx < 4; i_xx = i_xx + 1) begin : U_wire_xx
+        assign w_shift_in_xx[i_xx] = {r_shift_in, i_xx[1:0]};
+    end
+    // for(i_xxx = 0 ; i_xxx < 8; i_xxx = i_xxx + 1) begin : U_wire_xxx
+    //     assign w_shift_in_xxx[i_xxx] = {r_shift_in, i_xxx[2:0]};
+    // end
+    for(i_xdxx = 0 ; i_xdxx < 4; i_xdxx = i_xdxx + 1) begin : U_wire_xdxx
+        assign w_shift_in_xdxx[i_xdxx] = {r_shift_in_0dx, i_xdxx[1:0]};
+        assign w_shift_in_xdxx[i_xdxx + 4] = {r_shift_in_1dx, i_xdxx[1:0]};
+    end
+    endgenerate
+
+    assign w_shift_in_xdx[2'b00] = {r_shift_in_0dx, 1'b0};
+    assign w_shift_in_xdx[2'b01] = {r_shift_in_0dx, 1'b1};
+    assign w_shift_in_xdx[2'b10] = {r_shift_in_1dx, 1'b0};
+    assign w_shift_in_xdx[2'b11] = {r_shift_in_1dx, 1'b1};
 
     /********************************************************************************/
     /* END Assign */
+    /********************************************************************************/
+
+    /********************************************************************************/
+    /* Process decoding */
     /********************************************************************************/
 
     always @(posedge i_clk) begin
@@ -117,84 +135,140 @@ module viterbi_dec #(parameter p_size_polinom = 3, parameter p_polinom_0 = 3'b11
         begin
             r_mod_in_0 <= {p_defoult_in_0_1, p_defoult_in_0_0};
             r_mod_in_1 <= {p_defoult_in_1_1, p_defoult_in_1_0};
+            r_flag_error <= 0;
+            r_shift_in <= p_defoult_state;
+            r_shift_in_0dx <= p_defoult_state;
+            r_shift_in_1dx <= p_defoult_state;
         end
         else
         begin
             if(i_valid && !r_flag_error)
-            begin
+            begin : _defoult_decoding
                 if(w_idata == (r_mod_in_1 & i_valid))
                 begin
                     r_shift_in <= w_shift_in_1;
-                    r_mod_in_0[0] <= ^(w_shift_in_10 & p_polinom_0);
-                    r_mod_in_0[1] <= ^(w_shift_in_10 & p_polinom_1);
-                    r_mod_in_1[0] <= ^(w_shift_in_11 & p_polinom_0);
-                    r_mod_in_1[1] <= ^(w_shift_in_11 & p_polinom_1);
-                    r_odata <= 1;
+                    r_mod_in_0[0] <= ^(w_shift_in_xx[2'b10] & p_polinom_0);
+                    r_mod_in_0[1] <= ^(w_shift_in_xx[2'b10] & p_polinom_1);
+                    r_mod_in_1[0] <= ^(w_shift_in_xx[2'b11] & p_polinom_0);
+                    r_mod_in_1[1] <= ^(w_shift_in_xx[2'b11] & p_polinom_1);
+                    r_odata[0] <= 1;
                 end
                 else if(w_idata == (r_mod_in_0 & i_valid))
                 begin
                     r_shift_in <= w_shift_in_0;
-                    r_mod_in_0[0] <= ^(w_shift_in_00 & p_polinom_0);
-                    r_mod_in_0[1] <= ^(w_shift_in_00 & p_polinom_1);
-                    r_mod_in_1[0] <= ^(w_shift_in_01 & p_polinom_0);
-                    r_mod_in_1[1] <= ^(w_shift_in_01 & p_polinom_1);
-                    r_odata <= 0;
+                    r_mod_in_0[0] <= ^(w_shift_in_xx[2'b00] & p_polinom_0);
+                    r_mod_in_0[1] <= ^(w_shift_in_xx[2'b00] & p_polinom_1);
+                    r_mod_in_1[0] <= ^(w_shift_in_xx[2'b01] & p_polinom_0);
+                    r_mod_in_1[1] <= ^(w_shift_in_xx[2'b01] & p_polinom_1);
+                    r_odata[0] <= 0;
                 end
                 else 
                 begin : _find_error
-                    r_flag_error <= 1'b1;
-                    r_mod_in_00[0] <= ^(w_shift_in_00 & p_polinom_0);
-                    r_mod_in_00[1] <= ^(w_shift_in_00 & p_polinom_1);
-                    r_mod_in_01[0] <= ^(w_shift_in_01 & p_polinom_0);
-                    r_mod_in_01[1] <= ^(w_shift_in_01 & p_polinom_1);
-                    r_mod_in_10[0] <= ^(w_shift_in_10 & p_polinom_0);
-                    r_mod_in_10[1] <= ^(w_shift_in_10 & p_polinom_1);
-                    r_mod_in_11[0] <= ^(w_shift_in_11 & p_polinom_0);
-                    r_mod_in_11[1] <= ^(w_shift_in_11 & p_polinom_1);
+                    r_flag_error <= 1;
+
+                    r_shift_in_0dx <= w_shift_in_0;
+                    r_shift_in_1dx <= w_shift_in_1;
+
+                    r_mod_in_0d0 <= {0, ^(w_shift_in_xx[2'b00] & p_polinom_1), ^(w_shift_in_xx[2'b00] & p_polinom_0)};
+                    r_mod_in_0d1 <= {0, ^(w_shift_in_xx[2'b01] & p_polinom_1), ^(w_shift_in_xx[2'b01] & p_polinom_0)};
+                    r_mod_in_1d0 <= {0, ^(w_shift_in_xx[2'b10] & p_polinom_1), ^(w_shift_in_xx[2'b10] & p_polinom_0)};
+                    r_mod_in_1d1 <= {0, ^(w_shift_in_xx[2'b11] & p_polinom_1), ^(w_shift_in_xx[2'b11] & p_polinom_0)};
+
+                    r_decod_in_0dx <= 0;
+                    r_decod_in_1dx <= 1;
+
+                    r_in_data_old <= 0;
+                    r_in_data_mask <= 0;
                 end
             end
             else if(i_valid && r_flag_error)
             begin : _fix_error
-                r_flag_error <= 1'b0;
-                if(w_idata == (r_mod_in_11 & i_valid))
+                if(i_valid == 2'b11)//if come 2 bytes so i can fix error
                 begin
-                    r_shift_in <= w_shift_in_11;
-                    r_odata <= 1;
-                    r_odata_d1 <= 1;
-                    r_mod_in_0[0] <= ^(w_shift_in_110 & p_polinom_0);
-                    r_mod_in_0[1] <= ^(w_shift_in_110 & p_polinom_1);
-                    r_mod_in_1[0] <= ^(w_shift_in_111 & p_polinom_0);
-                    r_mod_in_1[1] <= ^(w_shift_in_111 & p_polinom_1);
+                    r_flag_error <= 0;
+                    if(w_in_data_shift == (r_mod_in_1d1 & w_in_mask_shift))
+                    begin
+                        r_shift_in <= w_shift_in_xdx[2'b11];
+                        r_odata <= {r_decod_in_1dx, 1'b1};
+                        r_mod_in_0[0] <= ^(w_shift_in_xdxx[3'b110] & p_polinom_0);
+                        r_mod_in_0[1] <= ^(w_shift_in_xdxx[3'b110] & p_polinom_1);
+                        r_mod_in_1[0] <= ^(w_shift_in_xdxx[3'b111] & p_polinom_0);
+                        r_mod_in_1[1] <= ^(w_shift_in_xdxx[3'b111] & p_polinom_1);
+                    end
+                    else if(w_in_data_shift == (r_mod_in_1d0 & w_in_mask_shift))
+                    begin
+                        r_shift_in <= w_shift_in_xdx[2'b10];
+                        r_odata <= {r_decod_in_1dx, 1'b0};
+                        r_mod_in_0[0] <= ^(w_shift_in_xdxx[3'b100] & p_polinom_0);
+                        r_mod_in_0[1] <= ^(w_shift_in_xdxx[3'b100] & p_polinom_1);
+                        r_mod_in_1[0] <= ^(w_shift_in_xdxx[3'b101] & p_polinom_0);
+                        r_mod_in_1[1] <= ^(w_shift_in_xdxx[3'b101] & p_polinom_1);
+                    end
+                    else if(w_in_data_shift == (r_mod_in_0d1 & w_in_mask_shift))
+                    begin
+                        r_shift_in <= w_shift_in_xdx[2'b01];
+                        r_odata <= {r_decod_in_0dx, 1'b1};
+                        r_mod_in_0[0] <= ^(w_shift_in_xdxx[3'b010] & p_polinom_0);
+                        r_mod_in_0[1] <= ^(w_shift_in_xdxx[3'b010] & p_polinom_1);
+                        r_mod_in_1[0] <= ^(w_shift_in_xdxx[3'b011] & p_polinom_0);
+                        r_mod_in_1[1] <= ^(w_shift_in_xdxx[3'b011] & p_polinom_1);
+                    end
+                    else
+                    begin
+                        r_shift_in <= w_shift_in_xdx[2'b00];
+                        r_odata <= {r_decod_in_0dx, 1'b0};
+                        r_mod_in_0[0] <= ^(w_shift_in_xdxx[3'b000] & p_polinom_0);
+                        r_mod_in_0[1] <= ^(w_shift_in_xdxx[3'b000] & p_polinom_1);
+                        r_mod_in_1[0] <= ^(w_shift_in_xdxx[3'b001] & p_polinom_0);
+                        r_mod_in_1[1] <= ^(w_shift_in_xdxx[3'b001] & p_polinom_1);
+                    end
                 end
-                else if(w_idata == (r_mod_in_10 & i_valid))
+                else //if come 1 bytes(speed more than 1/2) so I can't fix error and add in data to posible byffer and analize then
                 begin
-                    r_shift_in <= w_shift_in_10;
-                    r_odata <= 0;
-                    r_odata_d1 <= 1;
-                    r_mod_in_0[0] <= ^(w_shift_in_100 & p_polinom_0);
-                    r_mod_in_0[1] <= ^(w_shift_in_100 & p_polinom_1);
-                    r_mod_in_1[0] <= ^(w_shift_in_101 & p_polinom_0);
-                    r_mod_in_1[1] <= ^(w_shift_in_101 & p_polinom_1);
-                end
-                else if(w_idata == (r_mod_in_01 & i_valid))
-                begin
-                    r_shift_in <= w_shift_in_01;
-                    r_odata <= 1;
-                    r_odata_d1 <= 0;
-                    r_mod_in_0[0] <= ^(w_shift_in_010 & p_polinom_0);
-                    r_mod_in_0[1] <= ^(w_shift_in_010 & p_polinom_1);
-                    r_mod_in_1[0] <= ^(w_shift_in_011 & p_polinom_0);
-                    r_mod_in_1[1] <= ^(w_shift_in_011 & p_polinom_1);
-                end
-                else
-                begin
-                    r_shift_in <= w_shift_in_00;
-                    r_odata <= 0;
-                    r_odata_d1 <= 0;
-                    r_mod_in_0[0] <= ^(w_shift_in_000 & p_polinom_0);
-                    r_mod_in_0[1] <= ^(w_shift_in_000 & p_polinom_1);
-                    r_mod_in_1[0] <= ^(w_shift_in_001 & p_polinom_0);
-                    r_mod_in_1[1] <= ^(w_shift_in_001 & p_polinom_1);
+                    // TODO
+                    r_flag_error <= {r_flag_error, 1'b1};
+                    
+                    r_in_data_old <= {r_in_data_old, w_idata};
+                    r_in_data_mask <= {r_in_data_mask, i_valid};
+
+
+                    // //next posible data
+                    if(w_idata == (r_mod_in_0d1 & i_valid))         
+                        r_mod_in_0d0 <= {r_mod_in_0d0, /*r_mod_in_0d1[1:0],*/ ^(w_shift_in_xdxx[3'b010] & p_polinom_1), ^(w_shift_in_xdxx[3'b010] & p_polinom_0)};
+                    else                                            
+                        r_mod_in_0d0 <= {r_mod_in_0d0, ^(w_shift_in_xdxx[3'b000] & p_polinom_1), ^(w_shift_in_xdxx[3'b000] & p_polinom_0)};
+
+                    if(w_idata == (r_mod_in_0d1 & i_valid))         
+                        r_mod_in_0d1 <= {r_mod_in_0d1, ^(w_shift_in_xdxx[3'b011] & p_polinom_1), ^(w_shift_in_xdxx[3'b011] & p_polinom_0)};
+                    else                                            
+                        r_mod_in_0d1 <= {r_mod_in_0d1, /*r_mod_in_0d0[1:0],*/ ^(w_shift_in_xdxx[3'b001] & p_polinom_1), ^(w_shift_in_xdxx[3'b001] & p_polinom_0)};
+
+                    if(w_idata == (r_mod_in_1d1 & i_valid))         
+                        r_mod_in_1d0 <= {r_mod_in_1d0, /*r_mod_in_1d1[1:0],*/ ^(w_shift_in_xdxx[3'b110] & p_polinom_1), ^(w_shift_in_xdxx[3'b110] & p_polinom_0)};
+                    else                                            
+                        r_mod_in_1d0 <= {r_mod_in_1d0, ^(w_shift_in_xdxx[3'b100] & p_polinom_1), ^(w_shift_in_xdxx[3'b100] & p_polinom_0)};
+
+                    if(w_idata == (r_mod_in_1d1 & i_valid))         
+                        r_mod_in_1d1 <= {r_mod_in_1d1, ^(w_shift_in_xdxx[3'b111] & p_polinom_1), ^(w_shift_in_xdxx[3'b111] & p_polinom_0)};
+                    else                                            
+                        r_mod_in_1d1 <= {r_mod_in_1d1, /*r_mod_in_1d0[1:0],*/ ^(w_shift_in_xdxx[3'b101] & p_polinom_1), ^(w_shift_in_xdxx[3'b101] & p_polinom_0)};
+
+
+                    //decod shift
+                    if(w_idata == (r_mod_in_0d1 & i_valid))         r_shift_in_0dx <= {r_shift_in_0dx, 1'b1};
+                    else                                            r_shift_in_0dx <= {r_shift_in_0dx, 1'b0};
+
+                    if(w_idata == (r_mod_in_1d1 & i_valid))         r_shift_in_1dx <= {r_shift_in_1dx, 1'b1};
+                    else                                            r_shift_in_1dx <= {r_shift_in_1dx, 1'b0};
+
+
+                    //decod val
+                    if(w_idata == (r_mod_in_0d1 & i_valid))         r_decod_in_0dx <= {r_decod_in_0dx, 1'b1};
+                    else                                            r_decod_in_0dx <= {r_decod_in_0dx, 1'b0};
+
+                    if(w_idata == (r_mod_in_1d1 & i_valid))         r_decod_in_1dx <= {r_decod_in_1dx, 1'b1};
+                    else                                            r_decod_in_1dx <= {r_decod_in_1dx, 1'b0};
+
                 end
             end
         end
@@ -209,22 +283,17 @@ module viterbi_dec #(parameter p_size_polinom = 3, parameter p_polinom_0 = 3'b11
         begin
             if(i_valid)
             begin
-                if(w_idata == (r_mod_in_0 & i_valid))           r_valid <= 1;
-                else if(w_idata == (r_mod_in_1 & i_valid))      r_valid <= 1;
-                else if(r_flag_error)                           r_valid <= 1;
-                else                                            r_valid <= 0;
+                if(r_flag_error && i_valid[0] && i_valid[1])        r_valid <= {r_flag_error, 1'b1};
+                else if(w_idata == (r_mod_in_0 & i_valid))          r_valid <= !r_flag_error;
+                else if(w_idata == (r_mod_in_1 & i_valid))          r_valid <= !r_flag_error;
+                else                                                r_valid <= 0;
             end
             else    r_valid <= 0;
         end
     end
 
-    always @(posedge i_clk) begin
-        if(i_reset)                                 r_valid_d1 <= 0;
-        else if(i_valid && r_flag_error)            r_valid_d1 <= 1;    
-             else                                   r_valid_d1 <= 0;
-    end
-
-
-
+    /********************************************************************************/
+    /* END Process decoding */
+    /********************************************************************************/
 
 endmodule
--- tb/tb_viterbi_enc.v
+++ tb/tb_viterbi_enc.v
@@ -31,7 +31,8 @@ localparam size = 11;
 reg [size-1:0] modData = 11'b11001111010;
 reg [size+1:0] polinimError_0 = 13'b00000000000;
 reg [size+1:0] polinimError_1 = 13'b00000000000;
-reg [size*2+4:0] polinimError = 28'b0000000000000001001000;
+// reg [size*2+4:0] polinimError = 28'b0000000000000001001000;
+reg [size*2+4:0] polinimError = 28'b0000000000000000100000;
 reg valid = 0;
 wire validData;
 wire [1:0] outData;
@@ -93,11 +94,11 @@ viterbi_dec #(.p_size_polinom(7), .p_polinom_0(7'b1001111), .p_polinom_1(7'b1101
     .i_reset(1'b0),
     .i_data(/*outData*/dataDecIn),
     .i_valid({validDecoderData, validDecoderData}),
-    .o_data(dec_data),
-    .o_valid(dec_valid),
-    .o_error(dec_error),
-    .o_data_d1(dec_data_d1),
-    .o_valid_d1(dec_valid_d1)
+    .o_data({dec_data_d1, dec_data}),
+    .o_valid({dec_valid_d1, dec_valid}),
+    .o_error(dec_error)
+    // .o_data_d1(dec_data_d1),
+    // .o_valid_d1(dec_valid_d1)
 );
 
 reg [size+1:0] resData = 0;
@@ -125,18 +126,20 @@ wire [1:0] speed_valid;
 viterbi_speed_map #(.p_auto_pol(1), .p_speed_size(3), .p_speed_pol0(3'b101), .p_speed_pol1(3'b011)) map (
     .i_clk(clk),
     .i_reset(1'b0),
-    .i_data(outData),
+    .i_data(/*outData*/dataDecIn),
     .i_valid(validData),
-    .i_speed(3),
+    .i_speed(2),
     .o_data(speed_data),
     .o_valid(speed_valid)
 );
 
-wire dec_valid2;
-wire dec_data2;
+wire [6:0] dec_valid2;
+wire [6:0] dec_data2;
 wire dec_valid_d12;
 wire dec_data_d12;
 wire dec_error2;
+
+
 viterbi_dec #(.p_size_polinom(7), .p_polinom_0(7'b1001111), .p_polinom_1(7'b1101101), .p_defoult_state(7'b0000000)) dec2(
     .i_clk(clk),
     .i_reset(1'b0),
@@ -144,10 +147,41 @@ viterbi_dec #(.p_size_polinom(7), .p_polinom_0(7'b1001111), .p_polinom_1(7'b1101
     .i_valid(speed_valid),
     .o_data(dec_data2),
     .o_valid(dec_valid2),
-    .o_error(dec_error2),
-    .o_data_d1(dec_data_d12),
-    .o_valid_d1(dec_valid_d12)
+    .o_error(dec_error2)
 );
+
+reg [size+1:0] resData2 = 0;
+reg [4:0] counter2 = 0;
+
+always @(posedge clk) 
+begin
+    if(dec_valid2[3])
+    begin
+        counter2 <= counter2 + 4;
+        resData2[counter2+3] <= dec_data2[0];
+        resData2[counter2+2] <= dec_data2[1];
+        resData2[counter2+1] <= dec_data2[2];
+        resData2[counter2+0] <= dec_data2[3];
+    end
+    else if(dec_valid2[2])
+    begin
+        counter2 <= counter2 + 3;
+        resData2[counter2+2] <= dec_data2[0];
+        resData2[counter2+1] <= dec_data2[1];
+        resData2[counter2+0] <= dec_data2[2];
+    end
+    else if(dec_valid2[1])
+    begin
+        counter2 <= counter2 + 2;
+        resData2[counter2+1] <= dec_data2[0];
+        resData2[counter2+0] <= dec_data2[1];
+    end
+    else if(dec_valid2[0])
+    begin
+        counter2 <= counter2 + 1;
+        resData2[counter2+0] <= dec_data2[0];
+    end
+end
 
 
 
